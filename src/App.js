07d0d545078d4d5db354f921e0887c514e062ce4
cabbagekobe <circlekdeath@gmail.com>
2022-02-03 17:10:22 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import GlobalStyles from "./globals/GlobalStyles";
+
+const App = () => {
+  return (
+    <h1>
+      <GlobalStyles />
+      <h1>h1 - GlobalStyles from App.js</h1>
+      <h2>h2 - GlobalStyles from App.js</h2>
+      <h3>h3 - GlobalStyles from App.js</h3>
+      <h4>h4 - GlobalStyles from App.js</h4>
+      <h5>h5 - GlobalStyles from App.js</h5>
+      <h6>h6 - GlobalStyles from App.js</h6>
+      <p>p - GlobalStyles from App.js</p>
+    </h1>
+  );
+};
+
+export default App;
